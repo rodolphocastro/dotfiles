b652385a7c6c0ec63f534c2a9a7f2aa64fb533fb
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-02-03 00:16:01 -0300

    feat: Add WindowsTerminal to dotfiles

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -32,6 +32,14 @@ Add-Symlink "${HOME}\.gitconfig" "${PSScriptRoot}\.gitconfig"
 Write-Host "Replacing Powershell Profile"
 Add-Symlink "${PROFILE}" "${PSScriptRoot}\powershell\Microsoft.PowerShell_profile.ps1"
 
+Write-Host "Attempting to Replace Windows Terminal settings"
+$StorePackages = "${HOME}\AppData\Local\Packages\*Microsoft.WindowsTerminal*"
+$WindowsTerminalDir = Get-ChildItem $StorePackages
+if ($WindowsTerminalDir) {
+    Write-Host "Found WindowsTerminal on ${WindowsTerminalDir}, creating symlink"
+    Add-Symlink "${WindowsTerminalDir}\LocalState\settings.json" "${PSScriptRoot}\terminal\settings.json"
+}
+
 Write-Host "If this is a really fresh install run install_softwares.ps1 to get going" -ForegroundColor Yellow
 Write-Host "If you see Powershell Profile errors you'll want to run ./powershell/setup/install_pwsh_modules.ps1 as well" -ForegroundColor Yellow
 Write-Host "Done!" -ForegroundColor Green
