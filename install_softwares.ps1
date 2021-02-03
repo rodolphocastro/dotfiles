7f68e545f9dab23ce00be03c1171d849b8fd3253
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-02-02 23:53:08 -0300

    housekeeping: Cleanup install_softwares.ps1

--- install_softwares.ps1
+++ install_softwares.ps1
@@ -1,7 +1,14 @@
+<#
+.SYNOPSIS
+    Checks if a Command is currently available on Powershell
+#>
 function Test-CommandExists {
+    [CmdletBinding()]
     param (
-        $testCommand
+        [Parameter(Mandatory)]
+        [string]$testCommand
     )
+    
     try {
         Get-Command $testCommand -ErrorAction Stop
         return $true
@@ -9,19 +16,6 @@ function Test-CommandExists {
     catch {
         return $false
     }
-}
-
-# Appends something to $PROFILE
-function Add-To-Profile {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [string]
-        $sourceFile
-    )
-    $fileContent = Get-Content $sourceFile
-    Add-Content $PROFILE "`n"
-    Add-Content $PROFILE -Value $fileContent
 }
 
 if (!(Test-CommandExists winget)) {
@@ -32,18 +26,12 @@ if (!(Test-CommandExists winget)) {
 
 # TODO: Replace search with install
 
-# Setup winget autocomplete
-Add-To-Profile ".\pwsh_setup\winget_autocomplete.ps1"
+&winget search -e --id Microsoft.PowerShell
 
 &winget search -e --id Git.Git
-
-&winget search -e --id Microsoft.PowerShell
 
 &winget search -e --id Microsoft.WindowsTerminal
 
 &winget search -e --id Microsoft.VisualStudioCode-User-x64
 
 &winget search -e --id Microsoft.dotnet
-
-# Setup dotnet autocomplete
-Add-To-Profile ".\pwsh_setup\dotnet_autocomplete.ps1"
