4e930964b0d61165b987598544e53b2687810e79
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-02-02 22:09:20 -0300

    feat: Adds autocomplete setup for winget and dotnet

---
+++ pwsh_setup/dotnet_autocomplete.ps1
@@ -0,0 +1,8 @@
+# src: https://docs.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
