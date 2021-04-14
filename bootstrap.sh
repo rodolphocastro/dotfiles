efd226e142f7ebb84ba80f094a8a0b5431a47606
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-04-13 23:27:12 -0300

    feat: Create bootstrap for nix environments

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,26 @@ echo "Starting up dotfiles bootstrap (nix flavored)"
 echo "This script will override some of your home files"
 echo "If you are okay with that complete the sentence below, ALL CAPS please..."
 read -p "LEEEEEEEROY " answer
-echo $answer
+if [ $answer != "JENKINS" ]
+then
+    echo "At least you have chicken 🐔"
+    return -1
+fi
+
+# user's .gitconfig
+echo "Setting up .gitconfig"
+ln -f -s ./.gitconfig ~/.gitconfig
+
+# user's .gitignore
+echo "Setting up .gitignore"
+ln -f -s ./.gitignore ~/.gitignore
+
+# user's .bashrc
+echo "Setting up bashrc"
+ln -f -s ./.bashrc ~/.bashrc
+
+# extra steps!
+echo "Bootstrap complete, if you see any bash errors remember that you might need to install extra dependencies"
+echo "If this is within a WSL environment you might want to link up your SSH files and other goodies"
+
+return 0
