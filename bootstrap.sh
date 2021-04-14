0cabd59dc9ecfa79db9d8da050322d59b71432c0
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-04-13 22:26:28 -0300

    feat: Scaffold bootstrap for nix

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+echo "Starting up dotfiles bootstrap (nix flavored)"
+echo "This script will override some of your home files"
+echo "If you are okay with that complete the sentence below, ALL CAPS please..."
+read -p "LEEEEEEEROY " answer
+echo $answer
